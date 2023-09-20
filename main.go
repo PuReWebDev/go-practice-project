3bf6cd820cfbc5e585e3f9ea023cc5e0aa3985f9
purewebdev@gmail.com <purewebdev@gmail.com>
2023-09-20 13:25:52 -0400

    added some vars, printf to specify output

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var num int = 100
+	var pi float64 = 3.1415926536
+
 	fmt.Println("Hello World")
+	fmt.Printf("num: %v type: %T \n", num, num)
+	fmt.Printf("pi: %v type: %T \n\n", pi, pi)
 }
