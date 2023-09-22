a2d56d6b081eca3cba5b0e2d2661554503c32377
purewebdev@gmail.com <purewebdev@gmail.com>
2023-09-22 10:55:41 -0400

    basic system commands and system information

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hello/go-practice-project
 
 go 1.21.1
+
+require github.com/zcalusic/sysinfo v1.0.1
--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+	"os/user"
+
+	"github.com/zcalusic/sysinfo"
+)
 
 func main() {
-	var num int = 100
-	var pi float64 = 3.1415926536
 
-	fmt.Println("Hello World")
-	fmt.Printf("num: %v type: %T \n", num, num)
-	fmt.Printf("pi: %v type: %T \n\n", pi, pi)
+	outCommand, err := exec.Command("ls", "-ahl").Output()
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	output := string(outCommand[:])
+	fmt.Println(output)
+
+	current, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if current.Uid != "0" {
+		// log.Fatal("requires superuser privilege")
+		fmt.Println("Probably should try doing this as Root")
+	}
+
+	var si sysinfo.SysInfo
+
+	si.GetSysInfo()
+
+	data, err := json.MarshalIndent(&si, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
 }
